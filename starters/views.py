69de463ff63df49d09514610cbe008e7d2cfd423
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2023-12-07 20:26:53 +0300

    Working with models

--- starters/views.py
+++ starters/views.py
@@ -3,7 +3,11 @@ from django.http import HttpResponse
 from .models import Feature
 
 def index(request):
-        return  render(request, 'index.html')
+        feature1 = Feature()
+        feature1.id = 0
+        feature1.name = 'Fast'
+        feature1.details = "Our service is quicky"
+        return  render(request, 'index.html', {'feature ': feature1})
 
 
 ##done with already 
