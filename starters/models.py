71fafa734d0b27c9eef0d1062893c77612066397
amcoolalphonce <82672024+amcoolalphonce@users.noreply.github.com>
2023-12-07 20:23:49 +0300

    name details to strings

--- starters/models.py
+++ starters/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Feature():
         id : int
-        name : string
-        details: string 
+        name : str
+        details: str 
